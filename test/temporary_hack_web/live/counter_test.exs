0d563ccf9293d8a59fe8cb4266330fc411dfd02b
Andreas Kasprzok <akasprzok@gmail.com>
2020-09-20 18:27:15 -0700

    Fix up all the tests

---
+++ test/temporary_hack_web/live/counter_test.exs
@@ -0,0 +1,11 @@
+defmodule TemporaryHackWeb.CounterTest do
+  use TemporaryHackWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+  test "disconnected and connected render", %{conn: conn} do
+    {:ok, page_live, disconnected_html} = live(conn, "/counter")
+    rendered_html = render(page_live)
+    assert disconnected_html =~ "Counter"
+    assert rendered_html =~ "0"
+  end
+end
