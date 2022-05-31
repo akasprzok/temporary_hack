d9a0b3876528566cec222c7e903933178185dd5a
Andreas Kasprzok <akasprzok@gmail.com>
2022-05-30 19:56:47 -0700

    move env

--- config/test.exs
+++ config/test.exs
@@ -8,7 +8,7 @@ import Config
 config :temporary_hack, TemporaryHack.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: System.get_env("POSTGRES_HOST", "localhost"),
+  hostname: "localhost"
   database: "temporary_hack_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
