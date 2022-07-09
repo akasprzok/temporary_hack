4874d56be6bb8280e3f3ee654b9dabe52122cea7
Andreas Kasprzok <akasprzok@gmail.com>
2022-07-08 18:23:31 -0700

    more fleshed out Github client

--- lib/temporary_hack/clients/github.ex
+++ lib/temporary_hack/clients/github.ex
@@ -8,6 +8,19 @@ defmodule TemporaryHack.Clients.Github do
     {"User-Agent", "TemporaryHack"}
   ]
   plug Tesla.Middleware.BasicAuth, [username: "akasprzok", password: System.fetch_env!("GITHUB_ACCESS_TOKEN")]
+  plug Tesla.Middleware.Logger
+  plug Tesla.Middleware.Telemetry
+  plug Tesla.Middleware.PathParams
+  plug Tesla.Middleware.Retry,
+    delay: 200,
+    max_retries: 5,
+    max_delay: 5_000,
+    jitter_factor: 0.2,
+    should_retry: fn
+      {:ok, %{status: status}} when status in [500] -> true
+      {:ok, _} -> false
+      {:error, _} -> true
+    end
 
   def zen do
     get("/zen")
@@ -15,5 +28,10 @@ defmodule TemporaryHack.Clients.Github do
 
   def repos do
     get("user/repos")
+  end
+
+  def repo(repo) do
+    params = [repo: repo]
+    get("/repos/akasprzok/:repo", opts: [path_params: params])
   end
 end
