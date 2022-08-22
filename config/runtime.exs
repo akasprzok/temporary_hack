057917820ec11a2aadf79c00709c3da4c98b37f2
Andreas Kasprzok <akasprzok@gmail.com>
2022-08-22 12:37:56 -0700

    fix websocket error

--- config/runtime.exs
+++ config/runtime.exs
@@ -44,7 +44,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "https://temporaryhack.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :temporary_hack, TemporaryHackWeb.Endpoint,
@@ -57,6 +57,7 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
+    check_origin: ["https://temporaryhack.com"],
     secret_key_base: secret_key_base
 
   config :temporary_hack, TemporaryHack.Mailer,
